341505ec20843dd88ce3dd2db93c0e12b91dc108
Jay Ovalle <jay.ovalle@gmail.com>
2025-02-04 21:42:07 -0500

    Remove install confirmation

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -3,7 +3,7 @@
 . /etc/os-release
 
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/${ID}/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -15,4 +15,4 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose docker-compose-plugin
